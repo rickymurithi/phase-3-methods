152d4ad1bb875bb951a97f90ff4e104ce079be79
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-22 10:16:28 +0300

    add file

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+
+
+def greet (name="Naureen")
+    puts "Hello, #{name}!"
+end
+#greet("Naureen")
+
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+
+
+greet_with_default("Sunny")
+
+
+def add (num1, num2)
+    num1 + num2
+end
+
+
+def halve (num3)
+    return nil unless num3.class == Integer
+    num3/2
+end
+
+
